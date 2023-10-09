add7c8cf41e721736796dd0553e7ce8ec2528518
m0ar <edvard@hubinette.me>
2023-10-09 15:24:24 +0200

    Change component type enum to mimeType string

--- utils/templateData.d.ts
+++ utils/templateData.d.ts
@@ -11,7 +11,7 @@ export type ProfileTemplate = {
 export type ComponentTemplate = {
   name: string,
   dagNode: string,
-  type: string
+  mimeType: string
 };
 
 export type ResearchObjectTemplate = {
