0ae1932093eb1cdab6386ecf3eb16582363b5e73
m0ar <edvard@hubinette.me>
2023-11-02 12:40:13 +0100

    Add GHA reporter for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
+import GithubActionsReporter from "vitest-github-actions-reporter";
 
 export default defineConfig({
   test: {
     include: ["test/**/*.spec.ts"],
+    reporters: process.env.GITHUB_ACTIONS
+      ? ["default", new GithubActionsReporter()]
+      : "default",
   },
-  plugins: [tsconfigPaths()],
 });
